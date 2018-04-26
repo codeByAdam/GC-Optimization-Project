4cf55d2a48d59e1f0b457f22aff5a34889f08c68
Adam <aordway@vols.utk.edu>
2018-04-26 01:23:54 -0400

    testing

--- src/gcop/config/Driver.java
+++ src/gcop/config/Driver.java
@@ -22,36 +22,38 @@ public class Driver {
 
         //Start Iterations
         for(int i = 0; i < types.length; i++){
+            for(int j = 0; j < benchmarks.length; j++) {
 
-            List<String> cmd = new ArrayList<>();
+                List<String> cmd = new ArrayList<>();
 
-            cmd.add("java");
-            cmd.add("-XX:+" + types[i]);
-            cmd.add("-XX:+PrintGCTimeStamps");
-            cmd.add("-XX:+PrintGCDetails");
-            cmd.add("-XX:+PrintGCApplicationStoppedTime");
-            cmd.add("-XX:+PrintGCApplicationConcurrentTime");
-            cmd.add("-Xloggc:results/gc/" + types[i] + "_" + "derby.txt");
-            cmd.add("-jar");
-            cmd.add("SPECjvm2008.jar");
-            cmd.add("derby");
+                cmd.add("java");
+                cmd.add("-XX:+" + types[i]);
+                cmd.add("-XX:+PrintGCTimeStamps");
+                cmd.add("-XX:+PrintGCDetails");
+                cmd.add("-XX:+PrintGCApplicationStoppedTime");
+                cmd.add("-XX:+PrintGCApplicationConcurrentTime");
+                cmd.add("-Xloggc:results/gc/" + types[i] + "_" + benchmarks[j]);
+                cmd.add("-jar");
+                cmd.add("SPECjvm2008.jar");
+                cmd.add(benchmarks[j]);
 
-            System.out.println(cmd);
+                System.out.println(cmd);
 
-            ProcessBuilder pb = new ProcessBuilder(cmd);
-            try {
-                Process p = pb.start();
-                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String s = "";
-                while((s = in.readLine()) != null){
-                    System.out.println(s);
+                ProcessBuilder pb = new ProcessBuilder(cmd);
+                try {
+                    Process p = pb.start();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String s = "";
+                    while ((s = in.readLine()) != null) {
+                        System.out.println(s);
+                    }
+                    int status = p.waitFor();
+                    System.out.println("Exited with status: " + status);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException ee) {
+                    ee.printStackTrace();
                 }
-                int status = p.waitFor();
-                System.out.println("Exited with status: " + status);
-            }catch(IOException e){
-                e.printStackTrace();
-            }catch(InterruptedException ee){
-                ee.printStackTrace();
             }
 
         }
