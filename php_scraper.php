079b50e1876653b3ac77aa6412219df9413d1c90
Adam <aordway@vols.utk.edu>
2018-04-28 16:09:50 -0400

    new and improved scraper. Grabs more info

--- php_scraper.php
+++ php_scraper.php
@@ -6,6 +6,14 @@ $PATH = 'benchmarks/SPECjvm2008/results/gc/';
 $GC = [];
 
 //Regexes
+$rLabels = '/\[(?<label>\w+)\s?: (?<from>\d+(.\d+)?)K->(?<to>\d+(.\d+)?)K\((?<total>\d+(.\d+)?)K\)(, (?<ltime>\d+(.\d+)?) secs)?/';
+$rGCTime = '/\], (?<gctime>\d+(.\d+)?) secs\]/';
+$rTime = '/\[Times: user=(?<user>\d+(.\d+)?) sys=(?<sys>\d+(.\d+)?), real=(?<real>\d+(.\d+)?) secs\]/';
+/*$l = '15.189: [GC15.189: [DefNew: 37544K->4148K(37568K), 0.0114770 secs]15.200: [Tenured: 95846K->95862K(95872K), 0.0081260 secs] 100126K->99994K(133440K), [Perm : 5103K->5103K(21248K)], 0.0197350 secs] [Times: user=0.02 sys=0.01, real=0.02 secs] ';
+if(preg_match_all($rLabels, $l, $m)){
+	print_r($m);
+}*/
+
 $rDefNew = '/(?<flagTime>\d+(.\d+)?): \[GC\d+(.\d+): \[DefNew: (?<from>\d+(.\d+)?)K->(?<to>\d+(.\d+)?)K\((?<total>\d+(.\d+)?)K\), (?<time>\d+(.\d+)?) secs] (?<GCfrom>\d+(.\d+)?)K->(?<GCto>\d+(.\d+)?)K\((?<GCtotal>\d+(.\d+)?)K\), (?<GCtime>\d+(.\d+)?) secs\] \[Times: user=(?<userTime>\d+(.\d+)?) sys=(?<sysTime>\d+(.\d+)?), real=(?<realTime>\d+(.\d+)?) secs\]/';
 
 $files = scandir($PATH);
@@ -51,46 +59,59 @@ foreach($files as $file) {
 	//Loop through lines of file
 	while(! feof($f)){
 		$line = fgets($f);
-		$isGCRun = false;
+	
+		//If not GC line, move on
+		if(!preg_match('/GC/', $line)){
+			continue;
+		}
 
 		//Iterate counts
 		if(preg_match('/DefNew/', $line)){
 			$cDefNew++;
-			$isGCRun = true;
 		}
 		if(preg_match('/PSYoungGen/', $line)){
 			$cPSYoungGen++;
-			$isGCRun = true;
 		}
 		if(preg_match('/PSOldGen/', $line)){
 			$cPSOldGen++;
-			$isGCRun = true;
 		}
 		if(preg_match('/PSPermGen/', $line)){
 			$cPSPermGen++;
-			$isGCRun = true;
 		}
 
-		if(preg_match($rDefNew, $line, $matches)){
-			//print_r($matches);
-			array_push($obj['gc_runs'], [
-				'flagTime' => $matches['flagTime'],
-				'from' => $matches['from'],
-				'to' => $matches['to'],
-				'total' => $matches['total'],
-				'GCfrom' => $matches['GCfrom'],
-				'GCto' => $matches['GCto'],
-				'GCtotal' => $matches['GCtotal'],
-				'GCtime' => $matches['GCtime'],
-				'userTime' => $matches['userTime'],
-				'sysTime' => $matches['sysTime'],
-				'realTime' => $matches['realTime']
-			]);
+		/* 
+			Find GC Info in line
+		*/
+		
+		//make run object
+		$r = [];
+
+		if(preg_match_all($rLabels, $line, $matches)){
+
+			foreach($matches['label'] as $k => $label){
+				$r[$label] = [
+					'from' 	=> $matches['from'][$k],
+					'to'	=> $matches['to'][$k],
+					'total'	=> $matches['total'][$k],
+					'time' 	=> $matches['time'][$k],
+				];
+			}
+			
 		}
 
-		if($isGCRun){
-			$rc++;
+		if(preg_match($rGCTime, $line, $matches)){
+			$r['gctime'] = $matches['gctime'];
 		}
+
+		if(preg_match($rTime, $line, $matches)){
+			$r['user'] = $matches['user'];
+			$r['sys'] = $matches['sys'];
+			$r['real'] = $matches['real'];
+		}
+
+		//append run object to array of runs
+		array_push($obj['gc_runs'], $r);
+
 	}
 
 	//close file
@@ -107,13 +128,9 @@ foreach($files as $file) {
 	$f = fopen('GCObject.json', 'w');
 	fwrite($f, json_encode($GC));
 	fclose($f);
-	//$GC = json_decode($GC, JSON_PRETTY_PRINT);
-
-	//print_r($GC);
-
 	
-
-	//break;
+	//Break if you only want to run the frist file
+	break;
 }
 
 
