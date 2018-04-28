6e7855155b0f33ab24a63576282faf7621c76f83
Adam <aordway@vols.utk.edu>
2018-04-28 10:59:53 -0400

    parser gets some info

--- php_scraper.php
+++ php_scraper.php
@@ -2,12 +2,8 @@
 
 $PATH = 'benchmarks/SPECjvm2008/results/gc/';
 
-//Data Objects
-$GC = [
-	'DefNew' => [
-
-	],
-];
+//Data Object
+$GC = [];
 
 //Regexes
 $rDefNew = '/(?<flagTime>\d+(.\d+)?): \[GC\d+(.\d+): \[DefNew: (?<from>\d+(.\d+)?)K->(?<to>\d+(.\d+)?)K\((?<total>\d+(.\d+)?)K\), (?<time>\d+(.\d+)?) secs] (?<GCfrom>\d+(.\d+)?)K->(?<GCto>\d+(.\d+)?)K\((?<GCtotal>\d+(.\d+)?)K\), (?<GCtime>\d+(.\d+)?) secs\] \[Times: user=(?<userTime>\d+(.\d+)?) sys=(?<sysTime>\d+(.\d+)?), real=(?<realTime>\d+(.\d+)?) secs\]/';
@@ -15,40 +11,69 @@ $rDefNew = '/(?<flagTime>\d+(.\d+)?): \[GC\d+(.\d+): \[DefNew: (?<from>\d+(.\d+)
 $files = scandir($PATH);
 foreach($files as $file) {
 
-	$cDefNew = 0;
-	$cPSYoungGen = 0;
-	$cPSOldGen = 0;
-	$cPSPermGen = 0;
-	
 	//Don't open . and .. dirs
 	if($file[0] == "." || $file == ".."){
 		continue;
 	}
 
+	//Get benchmark name and GC algorithm
+	$names = explode("_", $file);
+	$ben = $names[1];
+	$com = $names[0];
+	
+	//Create Object to store benchmark data
+	if(!isset($GC[$ben])){
+		$GC[$ben] = [];
+	}
+	if(!isset($GC[$ben][$com])){
+		$GC[$ben][$com] = [];
+	}
+
+	//Nickname it $obj so can be uses easier
+	$obj = &$GC[$ben][$com];
+
+	//make array for runs
+	$obj['gc_runs'] = [];
+	
+	//Set counters
+	$cDefNew = 0;
+	$cPSYoungGen = 0;
+	$cPSOldGen = 0;
+	$cPSPermGen = 0;
+	
 	//Open File
 	$f = fopen($PATH . $file, "r");
-	echo $PATH . $file . "\n";
+	//echo $PATH . $file . "\n";
 	
+	//set run count
+	$rc = 0;
+
 	//Loop through lines of file
 	while(! feof($f)){
 		$line = fgets($f);
+		$isGCRun = false;
 
+		//Iterate counts
 		if(preg_match('/DefNew/', $line)){
 			$cDefNew++;
+			$isGCRun = true;
 		}
 		if(preg_match('/PSYoungGen/', $line)){
 			$cPSYoungGen++;
+			$isGCRun = true;
 		}
 		if(preg_match('/PSOldGen/', $line)){
 			$cPSOldGen++;
+			$isGCRun = true;
 		}
 		if(preg_match('/PSPermGen/', $line)){
 			$cPSPermGen++;
+			$isGCRun = true;
 		}
 
 		if(preg_match($rDefNew, $line, $matches)){
 			//print_r($matches);
-			array_push($GC['DefNew'], [
+			array_push($obj['gc_runs'], [
 				'flagTime' => $matches['flagTime'],
 				'from' => $matches['from'],
 				'to' => $matches['to'],
@@ -59,24 +84,34 @@ foreach($files as $file) {
 				'GCtime' => $matches['GCtime'],
 				'userTime' => $matches['userTime'],
 				'sysTime' => $matches['sysTime'],
-				'realTime' => $matches['realTime'],
+				'realTime' => $matches['realTime']
 			]);
 		}
 
+		if($isGCRun){
+			$rc++;
+		}
 	}
 
 	//close file
 	fclose($f);
 
-	$GC = json_encode($GC);
-	$GC = json_decode($GC, JSON_PRETTY_PRINT);
+	//Set stats in obj
+	$obj['stats'] = [
+		'DefNew_count' => $cDefNew,
+		'PSYoungGen_count' => $cPSYoungGen,
+		'PSOldGen_count' => $cPSOldGen,
+		'PSPermGen_count' => $cPSPermGen
+	];
+
+	$f = fopen('GCObject.json', 'w');
+	fwrite($f, json_encode($GC));
+	fclose($f);
+	//$GC = json_decode($GC, JSON_PRETTY_PRINT);
 
 	//print_r($GC);
 
-	printf("DefNew: %d\n", $cDefNew);
-	printf("PSYoungGen: %d\n", $cPSYoungGen);
-	printf("PSOldGen: %d\n", $cPSOldGen);
-	printf("PSPerGen: %d\n", $cPSPermGen);
+	
 
 	//break;
 }
