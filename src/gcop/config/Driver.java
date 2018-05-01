dd53a9bd90f316fc570ef5f91f5310a0882ac8c9
Adam <aordway@vols.utk.edu>
2018-05-01 18:05:14 -0400

    i think its fixed

--- src/gcop/config/Driver.java
+++ src/gcop/config/Driver.java
@@ -8,8 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutionException;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -60,7 +58,7 @@ public class Driver {
 
                 System.out.println(cmd);
 
-                /*ProcessBuilder pb = new ProcessBuilder(cmd);
+                ProcessBuilder pb = new ProcessBuilder(cmd);
                 try {
                     Process p = pb.start();
                     BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -74,7 +72,7 @@ public class Driver {
                     e.printStackTrace();
                 } catch (InterruptedException ee) {
                     ee.printStackTrace();
-                }*/
+                }
             }
 
         }
